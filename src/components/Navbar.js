e84f6d6eda77df540682d8e0ea4e297d33be6975
Jwaltz02 <jermaine.walters0ii@gmail.com>
2024-06-12 19:53:16 -0400

    Created navbar and landing page section

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,40 @@
 import React from "react";
+import logo from "../assets/logo-navbar.jpeg";
+import { Link } from "react-router-dom";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="bg-[#2B00AC] text-white h-32 border-b-2 border-gray-500 flex items-center">
+      <div className="gap-x-[10%] flex items-center pl-[58px]">
+        <img src={logo} width="17%" height="auto"></img>
+        <ul className="flex gap-[4vw] py-3">
+          <Link to="/Home">
+            <li className="py-2 hover:text-[#FF9900] hover:overline text-[1.1vw]">
+              Home
+            </li>
+          </Link>
+          <li className="py-2 hover:text-[#FF9900] hover:overline text-[1.1vw]">
+            <a href="#!">About Us</a>
+          </li>
+          <li className="py-2 hover:text-[#FF9900] hover:overline text-[1.1vw]">
+            <a href="#!">Services</a>
+          </li>
+          <li className="py-2 hover:text-[#FF9900] hover:overline text-[1.1vw]">
+            <a href="#!">Pricing</a>
+          </li>
+          <li className="py-2 hover:text-[#FF9900] hover:overline text-[1.1vw]">
+            <a href="#!">Portfolio</a>
+          </li>
+          <li className="py-2 hover:text-[#FF9900] hover:overline text-[1.1vw]">
+            <a href="#!">Contact Us</a>
+          </li>
+        </ul>
+      </div>
+      <button className="w-[15vw] h-[4vw] rounded-md border-2 border-white hover:bg-[#FF9900] hover:border-none mr-[58px] text-[1.1vw]">
+        Let's Talk
+      </button>
+    </div>
+  );
 }
 
 export default Navbar;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="bg-[#2B00AC]">
+      <div className="flex flex-col text-white text-left w-[26%] ml-[55px] pt-[100px] gap-y-[20px]">
+        <p className="text-[15px]">Digital Marketing Agency</p>
+        <h1 className="text-[59px]/[60px] font-bold">
+          Transform Your <span className="text-[#FF9900]">Business</span> Growth
+        </h1>
+        <p className="text-[17px]">
+          We provide innovative digital solutions to help your business succeed.
+        </p>
+        <button className="w-44 h-16 rounded-md bg-[#FF9900] font-bold">
+          Let's Discuss
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
