90b6000557842bf4fe4125b11b520128ba378dc2
Jwaltz02 <jermaine.walters0ii@gmail.com>
2024-06-13 06:51:47 -0400

    made the navbar more responsive with smaller screen sizes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,35 +4,37 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div className="bg-[#2B00AC] text-white h-32 border-b-2 border-gray-500 flex items-center">
-      <div className="gap-x-[10%] flex items-center pl-[58px]">
-        <img src={logo} width="17%" height="auto"></img>
-        <ul className="flex gap-[4vw] py-3">
-          <Link to="/Home">
+    <div className="bg-[#2B00AC] text-white h-[8vw] flex justify-center bg-[#2B00AC] pt-[14px]">
+      <div className="flex items-center w-[92%] border-b-2 border-gray-500">
+        <div className="flex items-center">
+          <img src={logo} width="20%" height="auto"></img>
+          <ul className="flex gap-[4vw] py-3 w-[100%] justify-center">
+            <Link to="/Home">
+              <li className="py-2 hover:text-[#FF9900] hover:overline text-[1.1vw]">
+                Home
+              </li>
+            </Link>
             <li className="py-2 hover:text-[#FF9900] hover:overline text-[1.1vw]">
-              Home
+              <a href="#!">About Us</a>
             </li>
-          </Link>
-          <li className="py-2 hover:text-[#FF9900] hover:overline text-[1.1vw]">
-            <a href="#!">About Us</a>
-          </li>
-          <li className="py-2 hover:text-[#FF9900] hover:overline text-[1.1vw]">
-            <a href="#!">Services</a>
-          </li>
-          <li className="py-2 hover:text-[#FF9900] hover:overline text-[1.1vw]">
-            <a href="#!">Pricing</a>
-          </li>
-          <li className="py-2 hover:text-[#FF9900] hover:overline text-[1.1vw]">
-            <a href="#!">Portfolio</a>
-          </li>
-          <li className="py-2 hover:text-[#FF9900] hover:overline text-[1.1vw]">
-            <a href="#!">Contact Us</a>
-          </li>
-        </ul>
+            <li className="py-2 hover:text-[#FF9900] hover:overline text-[1.1vw]">
+              <a href="#!">Services</a>
+            </li>
+            <li className="py-2 hover:text-[#FF9900] hover:overline text-[1.1vw]">
+              <a href="#!">Pricing</a>
+            </li>
+            <li className="py-2 hover:text-[#FF9900] hover:overline text-[1.1vw]">
+              <a href="#!">Portfolio</a>
+            </li>
+            <li className="py-2 hover:text-[#FF9900] hover:overline text-[1.1vw]">
+              <a href="#!">Contact Us</a>
+            </li>
+          </ul>
+        </div>
+        <button className="w-[260px] h-[4vw] scale-10 rounded-md border-2 border-white hover:bg-[#FF9900] hover:border-none hover:pl-[3px] text-[1.1vw]">
+          Let's Talk
+        </button>
       </div>
-      <button className="w-[15vw] h-[4vw] rounded-md border-2 border-white hover:bg-[#FF9900] hover:border-none mr-[58px] text-[1.1vw]">
-        Let's Talk
-      </button>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 function Home() {
   return (
     <div className="bg-[#2B00AC]">
-      <div className="flex flex-col text-white text-left w-[26%] ml-[55px] pt-[100px] gap-y-[20px]">
+      <div className="flex flex-col text-white text-left w-[26%] ml-[4%] pt-[100px] gap-y-[20px]">
         <p className="text-[15px]">Digital Marketing Agency</p>
         <h1 className="text-[59px]/[60px] font-bold">
           Transform Your <span className="text-[#FF9900]">Business</span> Growth
