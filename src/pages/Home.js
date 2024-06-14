dcbf1a867884a6ef86e7b48a7185bfd0c3824549
Jwaltz02 <jermaine.walters0ii@gmail.com>
2024-06-14 16:28:19 -0400

    added 'Our Portfolio' and 'Our Services' sections of the homepage in Home.js and made the webpages responsive

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,16 @@
 import React from "react";
-import Backdrop from "../assets/homepage-backdrop.jpeg";
 import "../styles/Home.css";
+import Backdrop from "../assets/homepage-backdrop.jpeg";
+import WhatWeDo from "../assets/what-we-do-homepage.jpeg";
+import { FaChartLine } from "react-icons/fa";
+import { BsTools } from "react-icons/bs";
+import { FaTags } from "react-icons/fa";
+import { MdSupportAgent } from "react-icons/md";
+import { PiArrowCircleRightLight } from "react-icons/pi";
+import { PiArrowCircleLeftLight } from "react-icons/pi";
+import LeftSideImagePortfolio from "../assets/left-side-image-portfolio.jpeg";
+import RightSideImagePortfolio from "../assets/right-side-image-portfolio.png";
+import { BsArrowRightShort } from "react-icons/bs";
 
 function Home() {
   return (
@@ -91,7 +101,10 @@ function Home() {
             <div className="w-1/6 border border-gray-400"></div>
           </div>
           <p className="hidden lg:block">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 568.02 201.81">
+            <svg
+              className="services-offered-line"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 568.02 201.81">
               <defs></defs>
               <g id="Calque_2" data-name="Calque 2">
                 <g id="Calque_3" data-name="Calque 3">
@@ -173,6 +186,109 @@ function Home() {
           </div>
         </div>
       </section>
+      <div className="bg-[#F4F4F4] pb-[5vw] pt-[4vw]">
+        <span className="text-[#2B00AC]">
+          <p className="text-[1.3vw] font-bold">Our Services</p>
+        </span>
+        <h1 className="text-[3.5vw] font-bold mt-[0.2vw] mb-[2vw]">
+          What We Do
+        </h1>
+        <div className="flex justify-center gap-x-[2.5vw] text-white">
+          <div className="flex flex-col w-[24vw] gap-y-[2.5vw]">
+            <div className="flex flex-col justify-center gap-y-[0.5vw] w-full h-[16vw] px-5 text-left bg-[#2B00AC] rounded-lg">
+              <FaChartLine className="w-[3vw] h-[3vw] text-[#FF9900]" />
+              <h1 className="text-[1.8vw] font-bold">Digital Marketing</h1>
+              <p className="text-[1.1vw]">
+                propel your business forward in the online landscape.
+              </p>
+            </div>
+            <div className="flex flex-col justify-center gap-y-[0.5vw] w-full h-[16vw] px-5 text-left bg-white rounded-lg text-black">
+              <BsTools className="w-[3vw] h-[3vw] text-[#FF9900]" />
+              <h1 className="text-[1.8vw] font-bold">Development</h1>
+              <p className="text-[1.1vw]">
+                dedicated to building robust, scalable, and high-performance
+                digital solutions.
+              </p>
+            </div>
+          </div>
+          <img src={WhatWeDo} className="w-[27vw] rounded-lg"></img>
+          <div className="flex flex-col w-[24vw] gap-y-[2.5vw]">
+            <div className="flex flex-col justify-center gap-y-[0.5vw] w-full h-[16vw] px-5 text-left bg-[#2B00AC] rounded-lg">
+              <FaTags className="w-[3vw] h-[3vw] text-[#FF9900]" />
+              <h1 className="text-[1.8vw] font-bold">Digital Marketing</h1>
+              <p className="text-[1.1vw]">
+                crafted to elevate your brand's visual identity and user
+                experience.
+              </p>
+            </div>
+            <div className="flex flex-col justify-center gap-y-[0.5vw] w-full h-[16vw] px-5 text-left bg-white rounded-lg text-black">
+              <MdSupportAgent className="w-[3vw] h-[3vw] text-[#FF9900]" />
+              <h1 className="text-[1.8vw] font-bold">Business</h1>
+              <p className="text-[1.1vw]">
+                Our business services are tailored to support and enhance your
+                company's growth and efficiency.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="bg-[#060606] text-white flex justify-center">
+        <div className="w-[80%] mt-[4vw]">
+          <p className="text-left text-[1.1vw] mb-[0.7vw] text-[#FF9900]">
+            OUR PORTFOLIO
+          </p>
+          <div className="flex gap-x-[14vw] items-center">
+            <h1 className="text-[3.5vw]/[4vw] text-left w-[50vw] font-bold">
+              Explore our new recently completed projects.
+            </h1>
+            <p className="w-[22vw] text-[1.1vw] text-left text-[#C8CFD5] float-right">
+              We are web designers, developers, project managers, and digital
+              solutions using the latest trends and technologies.
+            </p>
+          </div>
+          <div className="w-1/6 mt-[2vw] border border-gray-400"></div>
+          <div className="flex justify-end gap-x-[0.4vw] mt-[-1vw]">
+            <PiArrowCircleLeftLight className="text-[5.5vw] hover:fill-[#FF9900] cursor-pointer" />
+            <PiArrowCircleRightLight className="text-[5.5vw] hover:fill-[#FF9900] cursor-pointer" />
+          </div>
+          <div className="flex justify-center gap-x-[2vw] mt-[3vw] mb-[5vw] ">
+            <div className="w-[50vw] pb-[2vw] bg-white rounded-2xl">
+              <div className="flex w-full h-[29vw]">
+                <img
+                  src={LeftSideImagePortfolio}
+                  className="h-full object-cover rounded-t-2xl"></img>
+              </div>
+              <div className="flex justify-center gap-x-[8vw] items-center bg-white text-black h-[7vw]">
+                <div className="flex flex-col text-left">
+                  <h2 className="text-[2vw] font-bold">AfricaBlockChain</h2>
+                  <p className="text-[1.1vw]">Featured - Blockchain, Website</p>
+                </div>
+                <div className="w-[11vw] h-[3.5vw] rounded-md bg-[#FF9900] text-[1.1vw] flex items-center justify-center cursor-pointer">
+                  See Project
+                  <BsArrowRightShort className="text-[2vw]" />
+                </div>
+              </div>
+            </div>
+            <div className="w-[50vw] pb-[2vw] bg-white rounded-2xl">
+              <div className="flex w-full h-[29vw]">
+                <img
+                  src={RightSideImagePortfolio}
+                  className="h-full object-cover rounded-t-2xl"></img>
+              </div>
+              <div className="flex justify-center gap-x-[12vw] items-center text-black h-[7vw]">
+                <div className="flex flex-col text-left">
+                  <h2 className="text-[2vw] font-bold">AfroLifestyle</h2>
+                  <p className="text-[1.1vw]">Featured - eCommerce</p>
+                </div>
+                <div className="w-[11vw] h-[3.5vw] rounded-md bg-[#FF9900] text-[1.1vw] flex items-center justify-center cursor-pointer">
+                  See Project
+                  <BsArrowRightShort className="text-[2vw]" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
